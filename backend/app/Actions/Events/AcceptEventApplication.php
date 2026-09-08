e5fbecff306644fc3c32434d1d4118801fbb7b91
Michał Cichy <mcichy4@gmail.com>
2026-09-08 11:22:14 +0200

    feat: add event application acceptance

---
+++ backend/app/Actions/Events/AcceptEventApplication.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Actions\Events;
+
+use App\Enums\EventApplicationStatus;
+use App\Models\EventApplication;
+
+use DomainException;
+use Illuminate\Support\Facades\DB;
+
+class AcceptEventApplication
+{
+    public function execute(EventApplication $eventApplication): EventApplication
+    {
+
+        return DB::transaction(function () use ($eventApplication) {
+            $eventApplication = EventApplication::query()
+                ->lockForUpdate()
+                ->find($eventApplication->id);
+        
+                if($eventApplication->status !== EventApplicationStatus::PENDING) {
+                    throw new DomainException('Only pending applications can be accepted.');
+                }
+        
+                $event = $eventApplication->event
+                ->lockForUpdate()
+                ->firstOrFail();
+
+                if($event->isFinished()) {
+                    throw new DomainException('Cannot accept applications for finished events.');
+                }
+
+                if($event->participant_limit !== null) {
+                    $acceptedApplicationsCount = EventApplication::where('event_id', $event->id)
+                        ->where('status', EventApplicationStatus::ACCEPTED)
+                        ->count();
+        
+                    if($acceptedApplicationsCount >= $event->participant_limit) {
+                        throw new DomainException('Cannot accept application: participant limit reached.');
+                    }
+                };
+                $eventApplication->status = EventApplicationStatus::ACCEPTED;
+                $eventApplication->save();
+                return $eventApplication;
+                });
+
+
+
+    }
+}
---
+++ backend/tests/Feature/Actions/Events/AcceptEventApplicationTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Tests\Feature\Actions\Events;
+
+use App\Actions\Events\AcceptEventApplication;
+use App\Enums\EventApplicationStatus;
+use App\Enums\EventStatus;
+use App\Models\EventApplication;
+use App\Models\Event;   
+use App\Models\Organizer;
+use App\Models\User;
+use App\Enums\OrganizerType;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AcceptEventApplicationTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_pending_application_can_be_accepted(): void
+    {
+        $application = $this->createEventApplication();
+        // $application = EventApplication::factory()->create([
+        //     'status' => EventApplicationStatus::PENDING
+        // ]);
+
+        $result = app(AcceptEventApplication::class)->execute($application);
+
+        $this->assertSame(
+            EventApplicationStatus::ACCEPTED,
+            $result->status
+        );
+
+        $this->assertDatabaseHas('event_applications', [
+            'id' => $result->id,
+            'status' => EventApplicationStatus::ACCEPTED->value
+        ]);
+        }
+
+    public function test_non_pending_application_cannot_be_accepted(): void
+    {
+        $application = $this->createEventApplication(EventApplicationStatus::ACCEPTED);
+
+        $this->expectException(\DomainException::class);
+        app(AcceptEventApplication::class)->execute($application);
+    }
+    
+    public function test_application_cannot_be_accepted_for_finished_event(): void
+    {
+        $application = $this->createEventApplication(
+            EventApplicationStatus::PENDING,
+            null,
+            now()->subDays(2),
+            now()->subDay(),
+        );
+
+        $this->expectException(\DomainException::class);
+        app(AcceptEventApplication::class)->execute($application);
+    }
+
+    public function test_application_cannot_be_accepted_if_participant_limit_reached(): void
+    {
+        $application= $this->createEventApplication(participantLimit: 1);
+        EventApplication::forceCreate([
+            'event_id' => $application->event_id,
+            'status' => EventApplicationStatus::ACCEPTED,
+            'user_id' => User::factory()->create()->id
+        ]);
+
+        $this->expectException(\DomainException::class);
+        app(AcceptEventApplication::class)->execute($application);
+    }
+
+        private function createEventApplication(
+        EventApplicationStatus $status = EventApplicationStatus::PENDING,
+        ?int $participantLimit = null,
+        $startsAt = null,
+        $endsAt = null
+    ): EventApplication {
+        $user = User::factory()->create();
+
+        $organizer = Organizer::forceCreate([
+            'name' => 'Test Organizer',
+            'type' => OrganizerType::COMPANY,
+        ]);
+
+        $event = Event::forceCreate([
+            'organizer_id' => $organizer->id,
+            'title' => 'Test Event',
+            'description' => 'Test Description',
+            'starts_at' => $startsAt ?? now()->addDays(1),
+            'ends_at' => $endsAt ?? now()->addDays(2),
+            'location' => 'Test Location',
+            'participant_limit' => $participantLimit,
+            'status' => EventStatus::PUBLISHED,
+            ]);
+
+        return EventApplication::forceCreate([
+            'event_id' => $event->id,
+            'user_id' => $user->id,
+            'status' => $status,
+        ]);
+    }
+}

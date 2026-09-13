70b6188de2ac478b367596c612f315c546059012
Michal <mcichy4@gmail.com>
2026-09-13 09:28:09 +0200

    feat: add Sanctum session authentication

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
 // Startowa strona HTML z szablonu Laravel; frontend wydarzeń korzysta z osobnych tras API.
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
